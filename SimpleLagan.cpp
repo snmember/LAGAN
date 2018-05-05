7bde4b0f510605a99e2e52539c54897a47a44464
snmember <bashar.morouj@hotmail.de>
2018-05-05 18:24:11 +0200

    Update SimpleLagan.cpp

--- SimpleLagan.cpp
+++ SimpleLagan.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <seqan/arg_parse.h>
 #include <seqan/sequence.h>
 #include <seqan/seq_io.h>
 #include <seqan/stream.h>
@@ -15,24 +16,42 @@ typedef std::tuple<unsigned, size_t> QGram; //Qgram Tupel
 typedef Iterator<String<QGram>, Rooted>::Type QIter; //Rooted Iterator
 typedef Seed<Simple> TSeed; //Seed Klasse
 typedef SeedSet<TSeed> TSeedSet; //Seed Set
+typedef Iterator<TSeedSet, Rooted>::Type SIter;
 
 String<QGram> qGram1;
 String<QGram> qGram2; //Strings, welche die HashWerte des QGram Index beinhalten sollen.
 
 int main(int argc, char const *argv[]) {
-   if(argc != 3)
-    {
-      std::cerr << "Es sind zwei Fasta Dateien erforderlich!" << std::endl;
-    }
+
+   seqan::ArgumentParser parser("SimpleLagan");
+   addArgument(parser, seqan::ArgParseArgument(seqan::ArgParseArgument::STRING, "Path1"));
+   addArgument(parser, seqan::ArgParseArgument(seqan::ArgParseArgument::STRING, "Path2"));
+   addOption(parser, seqan::ArgParseOption("q", "seed", "Set the length of the seed",seqan::ArgParseArgument::INTEGER, "INT"));
+   setDefaultValue(parser, "seed", "10");
+   seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+   if (res != seqan::ArgumentParser::PARSE_OK)
+       return res == seqan::ArgumentParser::PARSE_ERROR;
+
+   unsigned q = 0;
+   getOptionValue(q, parser, "seed");
+   std::cout << q << std::endl;
+   CharString path1;
+   CharString path2;
+
+   getArgumentValue(path1, parser, 0);
+   getArgumentValue(path2, parser, 1);
+
    CharString id1;
    CharString id2;
    Dna5String seq1;
    Dna5String seq2;
 
-   SeqFileIn seqFileIn(toCString(argv[1]));
+
+   SeqFileIn seqFileIn(toCString(path1));
    readRecord(id1, seq1, seqFileIn);
 
-   SeqFileIn seqFileIn2(toCString(argv[2]));
+   SeqFileIn seqFileIn2(toCString(path2));
    readRecord(id2, seq2, seqFileIn2);
    //einlesen der Fasta Dateien
 
@@ -63,7 +82,7 @@ int main(int argc, char const *argv[]) {
    QIter it1 = begin(qGram1);
    QIter it2 = begin(qGram2); //setze Iteratoren jeweils an den Anfang meiner Strings
 
-   //std::cout << std::get<0>(*it1) << std::endl << std::get<0>(*it2); Das war ein Test, ob es tatsächlich Hash Werte ausgibt.
+   std::cout << std::get<0>(*it1) << std::endl << std::get<0>(*it2); //Das war ein Test, ob es tatsächlich Hash Werte ausgibt.
 
    while (!atEnd(it1) || !atEnd(it2)) //solange keiner der Iteratoren hinter dem letzten Element landet soll Loop ausgeführt werden
    {
@@ -87,6 +106,14 @@ int main(int argc, char const *argv[]) {
            goNext(it2);
        }
    }
+
+/*
+   Score<int, Simple> scoringScheme(1, -1, -1);
+   for (SIter it3 = begin(seedSet, Standard()); it3 != end(seedSet, Standard()); ++it3)
+       extendSeed(*it3, seq1, seq2, EXTEND_BOTH, scoringScheme, 5, GappedXDrop()); Der Teil funktioniert noch nicht
+*/
+
+
 
 
   return 0;
