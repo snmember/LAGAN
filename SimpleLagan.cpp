f21eee4476819df372e1a0b6b43da824e07a455b
snmember <bashar.morouj@hotmail.de>
2018-05-14 18:03:25 +0200

    Update SimpleLagan.cpp

--- SimpleLagan.cpp
+++ SimpleLagan.cpp
@@ -4,9 +4,9 @@
 #include <seqan/seq_io.h>
 #include <seqan/stream.h>
 #include <seqan/seeds.h>
-#include <seqan/sequence.h>
 #include <seqan/align.h>
 #include <seqan/score.h>
+#include <seqan/find.h>
 #include <algorithm>
 #include <string>
 using namespace seqan;
@@ -15,7 +15,7 @@ using namespace seqan;
 typedef std::tuple<unsigned long, size_t> QGram; //Qgram Tupel
 typedef Iterator<String<QGram>, Rooted>::Type QIter; //Rooted Iterator
 typedef Seed<Simple> TSeed; //Seed Klasse
-typedef SeedSet<TSeed> TSeedSet; //Seed Set
+typedef SeedSet<TSeed, Unordered> TSeedSet; //Seed Set
 typedef Iterator<TSeedSet>::Type SIter;
 
 String<QGram> qGram1;
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[]) {
 
    unsigned q = 0;
    getOptionValue(q, parser, "seed");
-   std::cout << q << std::endl;
+   std::cout << "Chosen seedlength: " << q << std::endl;
    CharString path1;
    CharString path2;
 
@@ -46,8 +46,8 @@ int main(int argc, char const *argv[]) {
 
    CharString id1;
    CharString id2;
-   Dna5String seq1;
-   Dna5String seq2;
+   DnaString seq1;
+   DnaString seq2;
 
 
    SeqFileIn seqFileIn(toCString(path1));
@@ -60,7 +60,7 @@ int main(int argc, char const *argv[]) {
 
    Shape<Dna, SimpleShape > myShape(q); //erstmals festgelegte Seedlänge, soll noch als optionale Eingabe modifiziert werden.
 
-
+   std::cout << "start to build QGram-Index!" << std::endl;
    hashInit(myShape, begin(seq1));
    for (size_t i = 0; i < length(seq1) - length(myShape) + 1; ++i)
        appendValue(qGram1, std::make_tuple(hashNext(myShape, begin(seq1) + i), i)) ;
@@ -68,6 +68,8 @@ int main(int argc, char const *argv[]) {
    hashInit(myShape, begin(seq2));
    for (size_t i = 0; i < length(seq2) - length(myShape) + 1; ++i)
        appendValue(qGram2, std::make_tuple(hashNext(myShape, begin(seq2) + i), i)) ; //QGram Index und Position in Tupel speichern.
+
+   std::cout << "Qgram index built, now sorting of the list begins" << std::endl;
 
    std::sort(begin(qGram1), end(qGram1), [] (QGram const & elem1, QGram const & elem2) // sortiert QGram Liste anhand des Hashwerts
    {
@@ -79,14 +81,20 @@ int main(int argc, char const *argv[]) {
        return std::get<0>(elem1) < std::get<0>(elem2);
    });
 
-   Score<int, Simple> scoringScheme(1, -1, -1);
+   Score<int, Simple> scoringScheme(1, -1, -1, -3);
    TSeedSet seedSet;
    QIter it1 = begin(qGram1);
    QIter it2 = begin(qGram2); //setze Iteratoren jeweils an den Anfang meiner Strings
    //QIter it_mem1;
-   QIter it_mem2;
-   std::cout << std::get<0>(*it1) << std::endl << std::get<0>(*it2) << std::endl; //Das war ein Test, ob es tatsächlich Hash Werte ausgibt.
+   QIter it_mem2 = begin(qGram2);
+   //std::cout << std::get<0>(*it1) << std::endl << std::get<0>(*it2) << std::endl; //Das war ein Test, ob es tatsächlich Hash Werte ausgibt.
 
+   std::cout << "Finding and extending of seeds starts!" << std::endl;
+
+   
+
+
+   unsigned long i = 0;
    while (!atEnd(it1) || !atEnd(it2)) //solange keiner der Iteratoren hinter dem letzten Element landet soll Loop ausgeführt werden
    {
        if(std::get<0>(*it1) > std::get<0>(*it2)) { goNext(it2); }
@@ -95,44 +103,37 @@ int main(int argc, char const *argv[]) {
 
        else
        {
-           //it_mem1 = it1;
+
            it_mem2 = it2;
            while(std::get<0>(*it1) == std::get<0>(*it2))
            {
                TSeed seed (std::get<1>(*it1), std::get<1>(*it2), q);
                extendSeed(seed, seq1, seq2, EXTEND_BOTH, MatchExtend());
                if(!addSeed(seedSet,seed, 2, 1, scoringScheme, seq1, seq2, Chaos()))
-                   addSeed(seedSet, seed ,Single());
+               addSeed(seedSet, seed ,Single());
                goNext(it2);
+               std::cout << "jo" << std::endl;
            }
-           it2 = it_mem2;
+           std::cout << i << std::endl;
+           i++;
            goNext(it1);
-        /*   while(std::get<0>(*it1) == std::get<0>(*it2))
-           {
-               TSeed seed (std::get<1>(*it1), std::get<1>(*it2), 9);
-               extendSeed(seed, seq1, seq2, EXTEND_BOTH, MatchExtend());
-               if(!addSeed(seedSet,seed, 2, Merge()))
-                   addSeed(seedSet, seed, Single());
-               goNext(it1);
-           }// den Teil muss ich noch etwas bearbeiten, da mehrere Fälle abgedeckt werden müssen.
-           goNext(it2);*/
+           it2 = it_mem2;
+
+
        }
    }
+          std::cout << "Chaining seeds now!" << std::endl;
 
+/*
 
-
-  /* for (SIter it3 = begin(seedSet, Standard()); it3 != end(seedSet, Standard()); ++it3)
-       {
-       TSeed myseed = *it3;
-       extendSeed(myseed, seq1, seq2, EXTEND_BOTH, scoringScheme, 5, GappedXDrop());
-     } */
    for (SIter it3 = begin(seedSet, Standard()); it3 != end(seedSet, Standard()); ++it3)
        std::cout << "Seed: " << *it3 << std::endl;
    std::cout << std::endl;
-
+*/
    String<TSeed> chainedSeeds;
    chainSeedsGlobally(chainedSeeds, seedSet, SparseChaining());
 
+   std::cout << "start to align sequences" << std::endl;
    Align<Dna5String, ArrayGaps> alignment;
    resize(rows(alignment), 2);
    assignSource(row(alignment, 0), seq1);
